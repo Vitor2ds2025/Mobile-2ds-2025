86f8db7aaa8183dc5646d7505e466eeb71284e7b
Vitor2ds2025 <vitor.krischanski@escola.pr.gov.br>
2025-04-25 10:15:59 -0300

    Add files via upload

---
+++ aula02.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Aula02 extends Component{
+    render(){
+        return(
+            <View style={{flex: 1, backgroundColor: 'black'}}>
+                <Text style={{color: 'white'}}> 
+                    AULA02 ESTILOS
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default Aula02;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'red'
+    }
+})
---
+++ aula03.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Aula03 extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Estrutura Padrão!</Text>
+            </View>
+        );
+    }
+}
+
+export default Aula03;
