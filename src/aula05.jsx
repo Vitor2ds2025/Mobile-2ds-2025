1e71070e798216b63b59a5658bac2fc8a18dcd61
Vitor2ds2025 <vitor.krischanski@escola.pr.gov.br>
2025-09-01 07:47:39 -0300

    Add files via upload

---
+++ src/aula05.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Aula05 extends Component{
+    render(){
+        return(
+            <View style={ styles.container}>
+                <View style={ styles.caixa1}>1</View>
+                <View style={ styles.caixa2}>
+                    <image/>
+                </View>
+                <View style={ styles.caixa3}>3</View>
+            </View>
+        );
+    }
+}
+
+export default Aula05;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+
+    caixa1: {
+        height: 100,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+        fontSize: 20,
+    },
+
+    caixa2: {
+       flex: 1, 
+       backgroundColor: 'green',
+       color: 'white',
+       alignItems: 'center',
+       justifyContent: 'center',
+       fontSize: 20
+    },
+
+    caixa3:{
+        backgroundColor: 'blue',
+        height: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 20,
+        color: 'white'
+    }
+})
